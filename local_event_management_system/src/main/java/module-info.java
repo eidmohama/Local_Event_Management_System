af8f08422c30050da25653300aa2d55b2cab11d8
Eid Mohammad Qasimi <134136282+eidmohama@users.noreply.github.com>
2025-12-18 14:31:27 +0100

    Clean up comments in module-info.java

--- local_event_management_system/src/main/java/module-info.java
+++ local_event_management_system/src/main/java/module-info.java
@@ -3,8 +3,7 @@ module local.event.management.system {
         requires javafx.fxml;
         requires java.sql;
 
-        // پکیج اصلی پروژه باید برای JavaFX export شود
-        exports org.example.local_event_management_system;
-        opens org.example.local_event_management_system to javafx.fxml; // برای FXML
-        opens org.example.local_event_management_system.db to javafx.base; // برای دسترسی db (اختیاری)
+               exports org.example.local_event_management_system;
+        opens org.example.local_event_management_system to javafx.fxml; 
+        opens org.example.local_event_management_system.db to javafx.base; 
         }
